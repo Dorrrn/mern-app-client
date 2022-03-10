0e970afaf983ef1b3aeed13b2687e0080f47bf70
dorrrn <doan7tran@gmail.com>
2022-03-10 12:49:55 +0100

    hide filter buttons when not logged in

--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -6,8 +6,7 @@ import axios from "axios";
 
 export default function Users(props) {
   const [foundUsers, setFoundUsers] = useState(props.users);
-  // const [filterUsers, setFilterUsers] = useState(undefined);
-  const { getToken } = useContext(AuthContext);
+  const { getToken, isLoggedIn } = useContext(AuthContext);
 
   const getAllUsers = () => {
     setFoundUsers(props.users);
@@ -25,41 +24,13 @@ export default function Users(props) {
       .catch((e) => console.log("error getting matches...", e));
   };
 
-  // useEffect(() => {
-  //   if (filterUsers) {
-  //     setFoundUsers(
-  //       props.users.filter((elm) => {
-  //         return elm.username
-  //           ?.toLowerCase()
-  //           .includes(filterUsers?.toLowerCase());
-  //       })
-  //     );
-  //   }
-  // }, [filterUsers]);
-
-  // const handleFilterUsers = (e) => {
-  //   if (e.target.value) {
-  //     e.preventDefault();
-  //     setFilterUsers(e.target.value);
-  //   } else {
-  //     setFilterUsers();
-  //   }
-  // };
-
-
   return (
     <div className="UsersPage">
       <h3>Find users to share your skills</h3>
+
+      {isLoggedIn && (
+        <>
       <div className="searchbar">
-        {/* <p>Search by name</p>
-        <form>
-          <input
-            type="text"
-            placeholder="search ... "
-            value={filterUsers}
-            onChange={handleFilterUsers}
-          />
-        </form> */}
         <button onClick={getAllUsers} className="all-users-button">
           See all users
         </button>
@@ -67,6 +38,8 @@ export default function Users(props) {
           See matches
         </button>
       </div>
+        </>
+      )}
 
       <div className="container">
         <div className="row justify-content-center">
@@ -88,9 +61,3 @@ export default function Users(props) {
     </div>
   );
 }
-
-//  <UserCards
-//           users={props.users}
-//           sliceStart="0"
-//           sliceEnd={props.users.length}
-//         />
